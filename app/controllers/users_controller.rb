b24ca2a6f576c0e36fa69d1ec3385b0a7226faee
JP Meyers <johnpmeyers20@yahoo.com>
2020-05-12 17:08:20 -0400

    added react

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :authorize_request, except: :create
   before_action :set_user, only: [:show, :update, :destroy]
+  before_action :authorize_request, except: :create
+  
   # GET /users
   def index
     @users = User.all

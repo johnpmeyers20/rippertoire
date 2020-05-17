3a3632eb958898e1092c8d313cf7cb2fcd2cfa6a
JP Meyers <johnpmeyers20@yahoo.com>
2020-05-17 00:13:25 -0400

    added button for editing individual songs

--- client/src/components/IndividualSong.jsx
+++ client/src/components/IndividualSong.jsx
@@ -7,6 +7,7 @@ function IndividualSong(props) {
   return (
     <div className='song-info'>
       <h2>{currentUser && props.match.params.song}</h2>
+      <button className='song-edit-button'>Edit Song</button>
       <p>{currentUser && songs.filter(song => song.title === props.match.params.song).map(song => song.lyrics)}</p>
     </div>
   )
